873b31dc3456f6dd4be37f98c8c21abad007d1ad
gagan6696 <sharma.gagan6696@gmail.com>
2019-01-10 12:49:36 +0530

    Initial Commit

---
+++ TestGagan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGagan
+//
+//  Created by Gagan on 1/10/19.
+//  Copyright © 2019 AditiSeasia Infotech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
